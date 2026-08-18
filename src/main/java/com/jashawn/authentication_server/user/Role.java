7015291bb4b12acd17cfd6c8ed796d9dd22a2352
JashawnRogers <rogersjashawn20@gmail.com>
2026-08-17 21:17:34 -0700

    feat(user): implemented UserDetails

---
+++ src/main/java/com/jashawn/authentication_server/user/Role.java
@@ -0,0 +1,6 @@
+package com.jashawn.authentication_server.user;
+
+public enum Role {
+    FREE,
+    PAID
+}
--- src/main/java/com/jashawn/authentication_server/user/User.java
+++ src/main/java/com/jashawn/authentication_server/user/User.java
@@ -5,6 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -12,7 +18,7 @@ import lombok.*;
 @Builder
 @Entity
 @Table(name = "_user") // underscore used to avoid naming conflict with postgresql auto generated user table
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -26,4 +32,40 @@ public class User {
 
     private String email;
 
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
