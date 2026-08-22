e5b5642b18f4a143c81cd36d0ad8190de301062f
JashawnRogers <rogersjashawn20@gmail.com>
2026-08-21 21:03:12 -0700

    feat(security): Add AuthenticationManager bean to app config

--- src/main/java/com/jashawn/authentication_server/config/ApplicationConfig.java
+++ src/main/java/com/jashawn/authentication_server/config/ApplicationConfig.java
@@ -2,10 +2,13 @@ package com.jashawn.authentication_server.config;
 
 import com.jashawn.authentication_server.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,6 +31,11 @@ public class ApplicationConfig {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(@NonNull AuthenticationConfiguration config) {
+        return config.getAuthenticationManager();
     }
 
     @Bean
